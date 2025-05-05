ead25a707694ada6f149c12b4084394ffbc9ffc4
Priyank <2priyankkumar@gmail.com>
2025-05-06 00:38:47 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
       <img src={assets.header_img} alt='header' className='w-64 h-64 md:w-48 md:h-48 rounded-full mb-6' />
       <h1 className='flex items-center text-3xl'>Hey Developer <img src={assets.hand_wave} alt='wave' className='w-8 aspect-square' /></h1>
       <h2 className='text-blue-950 text-5xl font-semibold leading-10'>Welcome to our app</h2>
-      <p className='text-center mt-2 text-md '>Let's start a quick tour and we will have you up and <br /> running in not ime!</p>
+      <p className='text-center mt-2 text-md '>Let's start a quick tour and we will have you up and <br /> running in no time!</p>
       <Button>Get Started</Button>
     </div>
   )
