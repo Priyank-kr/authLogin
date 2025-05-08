c032f4a26a65b627094f5977d328b19216a6edb2
Priyank <2priyankkumar@gmail.com>
2025-05-08 19:02:16 +0530

    j

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function Button({ children, login }) {
   return (
-    <button onClick={login} className="flex justify-center p-2 gap-2 px-6 rounded-full text-md text-gray-800 border border-gray-400 hover:bg-gray-300 cursor-pointer transition-all duration-200">
+    <button onClick={login} 
+    className="flex justify-center p-2 gap-2 px-6 rounded-full text-md text-indigo-950 border-b-2 border-gray-400 shadow-inner cursor-pointer bg-gradient-to-r from-indigo-100 to-indigo-100">
       {children}
     </button>
   );
