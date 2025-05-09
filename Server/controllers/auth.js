5f4f3101cc9183f5f2027fba0adbdeba568daaaa
Priyank <2priyankkumar@gmail.com>
2025-05-09 19:20:04 +0530

    Update auth.js

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -31,7 +31,7 @@ export const register = async (req, res) =>{
         res.cookie('token', token, {
             httpOnly : true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', 
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         
@@ -79,7 +79,7 @@ export const login = async (req, res) =>{
         res.cookie('token', token, {
             httpOnly : true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', 
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -98,7 +98,7 @@ export const logout = async (req, res) =>{
         res.clearCookie('token', {
             httpOnly : true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', 
         })
 
         return res.json({success: true, message: 'Logged out successfully'})
